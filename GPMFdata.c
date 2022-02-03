ad703926050b6b622cc289a3cc7a8125558b9b2c
destroyedlolo <l_faillie@yahoo.com>
2022-02-03 16:12:03 +0100

    Oups, correct speed normalisation

--- GPMFdata.c
+++ GPMFdata.c
@@ -26,13 +26,16 @@ double nextsample=-1;	/* Next sample timestamp to consider */
 double addSample( double sec, double lat, double lgt, double alt, double s2d, double s3d ){
 	double ret=0;
 
+	s2d *= 3.6;
+	s3d *= 3.6;
+
 		/* Min / Max */
 	if(nextsample < 0){	/* First data */
 		min.latitude = max.latitude = lat;
 		min.longitude = max.longitude = lgt;
 		min.altitude = max.altitude = alt;
-		min.spd2d = max.spd2d = s2d * 3.6;
-		min.spd3d = max.spd3d = s3d * 3.6;
+		min.spd2d = max.spd2d = s2d;
+		min.spd3d = max.spd3d = s3d;
 	} else {
 		if(lat < min.latitude)
 			min.latitude = lat;
@@ -50,14 +53,14 @@ double addSample( double sec, double lat, double lgt, double alt, double s2d, do
 			max.altitude = alt;
 
 		if(s2d < min.spd2d)
-			min.spd2d = s2d * 3.6;
+			min.spd2d = s2d;
 		if(s2d > max.spd2d)
-			max.spd2d = s2d * 3.6;
+			max.spd2d = s2d;
 
 		if(s3d < min.spd3d)
-			min.spd3d = s3d * 3.6;
+			min.spd3d = s3d;
 		if(s3d > max.spd3d)
-			max.spd3d = s3d * 3.6;
+			max.spd3d = s3d;
 	}
 
 		/* Store sample if needed */
@@ -85,8 +88,8 @@ double addSample( double sec, double lat, double lgt, double alt, double s2d, do
 		nv->latitude = lat;
 		nv->longitude = lgt;
 		nv->altitude = alt;
-		nv->spd2d = s2d * 3.6;
-		nv->spd3d = s3d * 3.6;
+		nv->spd2d = s2d;
+		nv->spd3d = s3d;
 	
 		samples_count++;
 		last = nv;
@@ -112,8 +115,8 @@ void dumpSample( void ){
 			printf("\tLatitude : %.3f deg\n", p->latitude);
 			printf("\tLongitude : %.3f deg\n", p->longitude);
 			printf("\tAltitude : %.3f m\n", p->altitude);
-			printf("\tSpeed2d : %.3f km/h\n", p->spd2d * 3.6);
-			printf("\tSpeed3d : %.3f km/h\n", p->spd3d * 3.6);
+			printf("\tSpeed2d : %.3f km/h\n", p->spd2d);
+			printf("\tSpeed3d : %.3f km/h\n", p->spd3d);
 		}
 	}
 
