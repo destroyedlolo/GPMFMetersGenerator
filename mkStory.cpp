4aaed36c65be05ff8574e918ea16ca1025c1bbc7
destroyedlolo <l_faillie@yahoo.com>
2022-05-22 01:33:29 +0200

    Add NamedGPVideo

--- mkStory.cpp
+++ mkStory.cpp
@@ -11,6 +11,7 @@
 #include <cstring>
 
 #include <vector>
+#include <iostream>
 
 #include "datalib/Context.h"
 #include "datalib/GPVideo.h"
@@ -18,23 +19,30 @@
 
 #define VERSION "0.01a01"
 
-GPX *gpx = NULL;	// external original GPX data
+GPX *Gpx = NULL;	// external original GPX data
 uint32_t proximity = 15;	// Proximity threshold 
 							// (bellow this distance, places are considered to 
 							// be the same.
-bool force = false;	// force video inclusion
 
-std::vector<GPVideo> videos;
+	/* Extended GPVideo with it's name */
+class NamedGPVideo : public GPVideo, public std::string {
+public:
+	NamedGPVideo( char *n ) : GPVideo(n), std::string(n){}
+};
+
+std::vector<NamedGPVideo> videos;
 
 int main(int argc, char *argv[]){
+	bool force = false;	// force video inclusion
+
 			/* Reading arguments */
 	int opt;
 	while(( opt = getopt(argc, argv, ":vdhG:p:F")) != -1){
 		switch(opt){
 		case 'G':
-			gpx = new GPX(optarg);
+			Gpx = new GPX(optarg);
 			if(verbose)
-				gpx->Dump();
+				Gpx->Dump();
 			break;
 		case 'd':	// debug implies verbose
 			debug = true;
@@ -69,7 +77,7 @@ int main(int argc, char *argv[]){
 		}
 	}
 
-	if(!gpx){
+	if(!Gpx){
 		fputs("No GPX file loaded\n", stderr);
 		exit(EXIT_FAILURE);
 	}
@@ -80,54 +88,21 @@ int main(int argc, char *argv[]){
 	}
 
 	for(; optind < argc; optind++){
-		printf("*I Reading '%s'\n", argv[optind]);
-
-			/* As GoPro's OpenMP4Source() needs a "char *",
-			 * it's easier to manage this temporary file in C
-			 * instead of a C++'s string
-			 */
-		char *fname = strdup(argv[optind]);
-		assert(fname);		// quick & dirty : no raison to fail
-
-		GPVideo video(argv[optind]);	// load the first chunk
-
-			// "GX013561.MP4" -> 12 chars
-		size_t len = strlen(fname);
-		if(len < 12){
-			fputs("*E* filename doesn't correspond to a GoPro video\n", stderr);
-			exit(EXIT_FAILURE);
-		}
-		if(strncmp(fname + len - 12, "GX01", 4)){
-			fputs("*E* not a GoPro video or not the 1st one\n", stderr);
-			exit(EXIT_FAILURE);
-		}
-		len -= 10;	// point to the part number
-	
-		for(unsigned int i = 2; i<99; i++){	// As per GoPro, up to 98 parts
-			char buff[3];
-			sprintf(buff, "%02d", i);
-			memcpy(fname + len, buff, 2);
-
-			if(!access(fname, R_OK)){
-				printf("*I Adding '%s'\n", argv[optind]);
-				
-				video.AddPart(fname);
-			}
-		}
+		NamedGPVideo video(argv[optind]);	// load the first chunk
 
 		if(verbose){
 			video.Dump();
 			printf("*I* Distance vs min : %.0fm, max %.0fm\n",
-				video.getMin().Distance(gpx->getMin()),
-				video.getMax().Distance(gpx->getMax())
+				video.getMin().Distance(Gpx->getMin()),
+				video.getMax().Distance(Gpx->getMax())
 			);
 		}
 
-		if( !gpx->sameArea(video.getMin()) ||
-			!gpx->sameArea(video.getMax()) ){
+		if( !Gpx->sameArea(video.getMin()) ||
+			!Gpx->sameArea(video.getMax()) ){
 				fprintf(stderr, "*W* This video seems outside the GPX trace (%.0fm and %.0fm)\n", 
-					video.getMin().Distance(gpx->getMin()),
-					video.getMax().Distance(gpx->getMax())
+					video.getMin().Distance(Gpx->getMin()),
+					video.getMax().Distance(Gpx->getMax())
 				);
 
 				if(!force){
@@ -136,8 +111,7 @@ int main(int argc, char *argv[]){
 				}
 		}
 		videos.push_back(video);
-
-		free(fname);
 	}
+
 }
 
